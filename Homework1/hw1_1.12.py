52f1c7172694e3bbfb2cc2b46c6de131f3060690
MohammadQu <60757189+MohammadQu@users.noreply.github.com>
2021-02-07 20:07:36 -0600

    Add files via upload

---
+++ Homework1/hw1_1.12.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+userNum = int(input())
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
+
+print(userNumSquared)
+# Output formatting issue here; fix it when instructed
+
+
