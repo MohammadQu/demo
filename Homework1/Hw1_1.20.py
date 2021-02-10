d9e9e39a8db2eb06449b419d621429ce123c9406
MohammadQu <60757189+MohammadQu@users.noreply.github.com>
2021-02-09 23:25:05 -0600

    Add files via upload

---
+++ Homework1/Hw1_1.20.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+print('Enter integer:')
+userNum = int(input())
+print('You entered: ',end='')
+print(userNum)
+userNumSquared = userNum * userNum
+print(userNum, 'squared is', end=' ')
+print(userNumSquared)
+userNumCubed = userNum * userNum * userNum
+print('And', userNum, 'cubed is', end=' ')
+print(userNumCubed, '!!')
+print('Enter another integer:')
+userNum2 = int(input())
+print(userNum, '+', userNum2, 'is', (userNum + userNum2))
+print(userNum, '*', userNum2, 'is', (userNum * userNum2))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
