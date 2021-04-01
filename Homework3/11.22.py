5c92b366f225d47290e57e9c072d6e53a0d3a340
MohammadQu <60757189+MohammadQu@users.noreply.github.com>
2021-03-31 23:10:33 -0500

    Add files via upload

---
+++ Homework3/11.22.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#Mohammad Qureshi
+#PSID: 1789301
+#11.22
+
+
+user_input = input().split()
+
+for x in user_input:
+    print(x,user_input.count(x))
